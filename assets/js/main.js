e4c1682f7455e0927a93e8ec65fa0feb2470b2f9
amit <akviitian@gmail.com>
2024-11-22 12:40:07 +0530

    clean code

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,13 +41,7 @@ function scrollActive(){
 }
 window.addEventListener('scroll', scrollActive)
 
-/*==================== SIDE SCROLL WORK IMAGES =============*/
-let scrollContainer = document.querySelector(".work__container");
 
-scrollContainer.addEventListener("wheel", (evt) => {
-    evt.preventDefault();
-    scrollContainer.scrollLeft += evt.deltaY;
-})
 
 /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
@@ -61,7 +55,6 @@ const sr = ScrollReveal({
 sr.reveal('.home__data, .about__img, .skills__subtitle, .skills__text',{}); 
 sr.reveal('.home__img, .about__subtitle, .about__text, .skills__img',{delay: 400}); 
 sr.reveal('.home__social-icon',{ interval: 200}); 
-sr.reveal('.skills__data, .work__img, .contact__input',{interval: 200}); 
 
 /*=========== CONTACT-FORM =======*/
 
